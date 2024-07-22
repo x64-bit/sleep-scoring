d26e3d864bd83c2d4a41607c41230521e6ff9897
Anjo Pagdanganan <anjopag31@berkeley.edu>
2024-07-21 21:44:06 -0700

    deleted 3d visualization objects and irrelevant tests

--- visbrain/visuals/__init__.py
+++ visbrain/visuals/__init__.py
@@ -1,5 +1,5 @@
 """Visual objects."""
-from .brain_visual import BrainMesh  # noqa
+# from .brain_visual import BrainMesh  # noqa
 from .cbar import *  # noqa
 from .grid_signal_visual import GridSignal  # noqa
 from .hypno_visual import Hypnogram  # noqa
